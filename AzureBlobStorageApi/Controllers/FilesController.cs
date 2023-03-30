e09d71c1b99585f095e3e3407e1defb5500086c9
TeeCee <nguyen.nguyen@spiderbox.design>
2023-03-30 10:17:54 +0700

    first commit

---
+++ AzureBlobStorageApi/Controllers/FilesController.cs
@@ -0,0 +1,51 @@
+﻿using AzureBlobStorageApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureBlobStorageApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilesController : Controller
+    {
+        private readonly FileService _fileService;
+
+        public FilesController(FileService fileService)
+        {
+            _fileService = fileService;
+        } 
+
+        [HttpGet]
+        public async Task<ActionResult> ListAllBlobs()
+        {
+            var result = await _fileService.ListAsync();
+            var type = result.GetType().ToString();
+            
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Upload(IFormFile file)
+        {
+            var result = await _fileService.UploadAsync(file);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("filename")]
+        public async Task<ActionResult> Download(string filename)
+        {
+            var result = await _fileService.DownloadAsync(filename);
+            return File(result.Content, result.ContentType, result.Name);
+        }
+
+        [HttpDelete]
+        [Route("filename")]
+        public async Task<ActionResult> Delete(string filename)
+        {
+            var result = await _fileService.DeleteAsync(filename);
+            return Ok(result);
+        }
+       
+    }
+}
---
+++ AzureBlobStorageApi/Services/FileService.cs
@@ -0,0 +1,89 @@
+﻿using Azure.Storage;
+using Azure.Storage.Blobs;
+using AzureBlobStorageApi.Dtos;
+
+namespace AzureBlobStorageApi.Services
+{
+    public class FileService
+    {
+        private readonly string _storageAccount = "nguyennguyenstorage";
+        private readonly string _key = "xU+KKXBNrRl1cBMId9NotMq/4irQCajxZTh+HmgQuVDTNbTz+Ce+ow/mZpWpRXAqjceXEwZjdD7h+AStX8c2Nw==";
+        private readonly BlobContainerClient _fileContainer;
+
+        public FileService()
+        {
+            var credential = new StorageSharedKeyCredential(_storageAccount, _key);
+            var blobUri = $"https://{_storageAccount}.blob.core.windows.net";
+            var blobServiceClient = new BlobServiceClient(new Uri(blobUri), credential);
+            _fileContainer = blobServiceClient.GetBlobContainerClient("nguyencontainer");
+        }
+
+        public async Task<List<BlobDto>> ListAsync()
+        {
+            List<BlobDto> files = new();
+
+            await foreach (var file in _fileContainer.GetBlobsAsync())
+            {
+                string uri = _fileContainer.Uri.ToString();
+                var name = file.Name;
+                var fullUri = $"{uri}/{name}";
+
+                files.Add(new BlobDto
+                {
+                    Uri = fullUri,
+                    Name = name,
+                    ContentType = file.Properties.ContentType,
+                });
+            }
+
+            return files;
+        }
+
+        public async Task<BlobResponseDto> UploadAsync(IFormFile blob)
+        {
+            BlobResponseDto response = new();
+            BlobClient client = _fileContainer.GetBlobClient(blob.FileName);
+
+            await using (Stream? data = blob.OpenReadStream())
+            {
+                await client.UploadAsync(data);
+            }
+
+            response.Status = $"File {blob.FileName} Uploaded Successfully";
+            response.Error = false;
+            response.Blob.Uri = client.Uri.AbsoluteUri;
+            response.Blob.Name = blob.Name;
+
+            return response;
+        }
+
+        public async Task<BlobDto?> DownloadAsync(String blobFileName)
+        {
+            BlobClient file = _fileContainer.GetBlobClient(blobFileName);
+
+            if(await file.ExistsAsync())
+            {
+                var data = await file.OpenReadAsync();
+                Stream blobContent = data;
+
+                var content = await file.DownloadContentAsync();
+
+                string name = blobFileName;
+                string contentType = content.Value.Details.ContentType;
+
+                return new BlobDto { Content = blobContent, Name = name, ContentType = contentType };
+            }
+
+            return null;
+        }
+
+        public async Task<BlobResponseDto> DeleteAsync(String blobFileName)
+        {
+            BlobClient file = _fileContainer.GetBlobClient(blobFileName);
+
+            await file.DeleteAsync();
+
+            return new BlobResponseDto { Error = false, Status = $"File: {blobFileName} has been successfully deleted" };
+        }
+    }
+}
